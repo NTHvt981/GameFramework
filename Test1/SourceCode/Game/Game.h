f6903db76b00c50a3b03ae2e4ff19e2823ab8265
Nguyễn Trung Hiếu <17520480@gm.uit.edu.vn>
2020-09-26 10:50:29 +0700

    Add Collision class add swept aabb function

--- Test1/SourceCode/Game/Game.h
+++ Test1/SourceCode/Game/Game.h
@@ -32,5 +32,21 @@ public:
 	void Run();
 
 	~CGame();
+
+	//vital functions for game object to use
+	static void SweptAABB(
+		float move_left,		
+		float move_top,			
+		float move_right,			
+		float move_bottom,			
+		float move_velocity_x,			
+		float move_velocity_y,			
+		float static_left,			
+		float static_top,
+		float static_right,
+		float static_bottom,
+		float& collide_time,
+		float& normal_x,
+		float& normal_y);
 };
 

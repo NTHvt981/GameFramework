5d792406becd18a8b7b3f016566cb3ebf42f60ec
Nguyen Trung Hieu <NTHvt981@gmail.com>
2022-10-08 15:17:01 +0700

    * Update IntroSceneScript

--- code/source/logic/include/Logic/Scripts/IntroSceneScript.h
+++ code/source/logic/include/Logic/Scripts/IntroSceneScript.h
@@ -7,6 +7,8 @@ namespace logic
 
 class Entity;
 class AnimationComponent;
+class AudioComponent;
+class CameraComponent;
 
 class IntroSceneScript: public SceneScript
 {
@@ -18,6 +20,8 @@ private:
 	void OnAnimationFinished();
 	std::shared_ptr<Entity> m_introEntity;
 	std::shared_ptr<AnimationComponent> m_animationComponent;
+	std::shared_ptr<AudioComponent> m_audioComponent;
+	std::shared_ptr<CameraComponent> m_cameraComponent;
 	signals::Connection<> m_onAnimationFinishedCon;
 };
 
--- code/source/logic/src/Scripts/IntroSceneScript.cpp
+++ code/source/logic/src/Scripts/IntroSceneScript.cpp
@@ -5,6 +5,9 @@
 #include "Logic/Factories/IEntitiesFactory.h"
 #include "Logic/Managers/EntitiesManager.h"
 #include "Logic/Components/AnimationComponent.h"
+#include "Logic/Components/AudioComponent.h"
+#include "Logic/Components/CameraComponent.h"
+#include "Core/Helpers/SizeHelper.h"
 
 namespace logic
 {
@@ -16,10 +19,19 @@ void IntroSceneScript::OnCreate(std::shared_ptr<IScriptContext> i_scriptContext)
 	m_introEntity->Register();
 
 	m_animationComponent = m_introEntity->GetComponent<AnimationComponent>(sk_animationComponentKey);
+	m_cameraComponent = m_introEntity->GetComponent<CameraComponent>(sk_cameraComponentKey);
+	m_audioComponent = m_introEntity->GetComponent<AudioComponent>(sk_audioComponentKey);
+
 	m_animationComponent->SetLoop(false);
 	m_onAnimationFinishedCon = m_animationComponent->sig_onAnimationFinished.Connect(
 		std::bind(&IntroSceneScript::OnAnimationFinished, this)
 	);
+
+	m_cameraComponent->Register();
+	m_cameraComponent->SetCameraSize(
+		core::CastSize<int64_t, float>(m_animationComponent->GetSpriteSizeInFrame(0))
+	);
+	m_cameraComponent->Deregister();
 }
 
 void IntroSceneScript::OnDestroy()

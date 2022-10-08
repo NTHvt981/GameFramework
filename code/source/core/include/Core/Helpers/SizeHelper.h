654e12e3898709978ace38f8f928a09075324c33
Nguyen Trung Hieu <NTHvt981@gmail.com>
2022-10-08 15:08:55 +0700

    + Add SizeHelper.h

---
+++ code/source/core/include/Core/Helpers/SizeHelper.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Core/DataTypes/Size.h"
+#include <math.h>
+
+namespace core
+{
+
+// Size param order
+// width -> height
+
+// declare
+template <typename T>
+Size<T> operator*(const Size<T>& a, const float b);
+template <typename T>
+Size<T> operator/(const Size<T>& a, const float b);
+template <typename V, typename T>
+Size<T> Convert(const Size<V>& a);
+
+// impl
+template <typename T>
+Size<T> operator*(const Size<T>& a, const float b)
+{
+	return Size<T>
+	{
+		a.width* b,
+		a.height* b
+	};
+}
+
+template <typename T>
+Size<T> operator/(const Size<T>& a, const float b)
+{
+	return Size<T>
+	{
+		a.width / b,
+		a.height / b
+	};
+}
+
+template<typename V, typename T>
+Size<T> Convert(const Size<V>& a)
+{
+	return Size<T>(
+		static_cast<T>(a.width),
+		static_cast<T>(a.height)
+	);
+}
+
+} // namespace core::string

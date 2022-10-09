be461ff049717d43eae7a493c79d7f737724bceb
Nguyen Trung Hieu <NTHvt981@gmail.com>
2022-10-09 22:07:39 +0700

    - Remove echo in copyData.bat

--- copyData.bat
+++ copyData.bat
@@ -5,7 +5,7 @@ SET "SRC=%cd%\%FOLDER%"
 SET "DEST=%cd%\release\%FOLDER%"
 
 @REM Xcopy "%SRC%\%FOLDER%" "%DEST%\%FOLDER%" /s /e /z /i /f /k
-Xcopy "%FOLDER%" "release\%CONFIG%\%FOLDER%" /s /e /z /i /f /k /y /v
+Xcopy "%FOLDER%" "release\%CONFIG%\%FOLDER%" /s /e /z /i /q /k /y /v
 
 ECHO "files from %SRC% to %DEST% finished."
 pause

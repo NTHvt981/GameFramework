3356beda95c6552d20f3321542be16c732c44fca
Nguyen Trung Hieu <NTHvt981@gmail.com>
2022-10-19 16:27:54 +0700

    - Remove test function

--- code/source/direct-wrapper/src/Audio/DirectAudioAPI.cpp
+++ code/source/direct-wrapper/src/Audio/DirectAudioAPI.cpp
@@ -10,17 +10,6 @@
 #include <assert.h>
 #include <dinput.h>
 
-// test function
-inline bool FileExist(const std::string& name) {
-    if (FILE* file = fopen(name.c_str(), "r")) {
-        fclose(file);
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
 namespace audios
 {
 
